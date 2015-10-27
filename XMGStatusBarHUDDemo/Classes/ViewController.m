fc6256ffb02da06dfb4891b6ab5b06f83eb2ce01
oneshunzi <123@qq.com>
2015-10-27 15:44:10 +0800

    删掉一些不必要的注释
    删掉一些不必要的注释

--- XMGStatusBarHUDDemo/Classes/ViewController.m
+++ XMGStatusBarHUDDemo/Classes/ViewController.m
@@ -17,20 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    
     
 }
 
 - (IBAction)success {
     [XMGStatusBarHUD showSuccess:@"登录成功!"];
-//    [XMGStatusBarHUD showText:@"哈哈哈哈"];
-//    [XMGStatusBarHUD showText:@"哈哈哈" imageName:@"test"];
-//    [XMGStatusBarHUD showText:@"4234" image:[UIImage imageNamed:@"test"]];
 }
 
 - (IBAction)eror {
@@ -46,10 +37,3 @@
 }
 
 @end
-
-/**
- 设计第三方框架的要点:
- 1.必须要有主头文件, 在主头文件中包含框架中的其他需要用到的头文件
- 
- 2.接口设计 : 思考需要哪些类\哪些方法
- */
